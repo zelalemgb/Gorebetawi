a5d8cf0bb60ad1f5b44c342511cdc2a323084795
zelalemgb <78460729+zelalemgb@users.noreply.github.com>
2025-06-05 12:39:13 +0300

    Fix SplashScreen Import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
-import { SplashScreen } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
