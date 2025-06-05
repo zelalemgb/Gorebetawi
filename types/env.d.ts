6fe13a99cf847a2e7c89ac8a2a2543c441ef5b52
zelalemgb <78460729+zelalemgb@users.noreply.github.com>
2025-06-05 14:32:57 +0300

    Configure Supabase Environment

---
+++ types/env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+  export const EXPO_PUBLIC_SUPABASE_URL: string;
+  export const EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
+}
+
+// Ensure this file is treated as a module
+export {};
