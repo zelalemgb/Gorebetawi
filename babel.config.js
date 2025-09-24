70d5a857478ded3653e202a01a14c2c35319dfc8
zelalemgb <78460729+zelalemgb@users.noreply.github.com>
2025-09-24 11:28:34 +0300

    Fix Application Running Issues

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'react' }]
+    ],
     plugins: [
       'expo-router/babel',
       'react-native-reanimated/plugin'
